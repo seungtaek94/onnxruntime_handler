0ba092f2d0807e0522b746dd5993c0d04db7080a
seungtaek94 <seungtaek.kim.94@gmail.com>
2022-09-22 17:05:13 +0900

    Add script for linux-x64 build.

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd onnxruntime
+wget https://github.com/microsoft/onnxruntime/releases/download/v1.12.1/onnxruntime-linux-x64-1.12.1.tgz
+tar -zxvf onnxruntime-linux-x64-1.12.1.tgz
+cd ../
+
+git clone -b release-1.12.1 https://github.com/google/googletest.git
+
+cmake -DCMAKE_BUILD_TYPE=Debug -S ./ -B ./cmake-build-debug-linux
+
+cmake --build cmake-build-debug-linux/ --target orthandler -j 2
+
+cmake --build cmake-build-debug-linux/ --target test_orthandler -j 2
