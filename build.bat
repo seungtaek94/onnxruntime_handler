e3f7740abfa83e06c5520bb084249c2a7d5ece98
seungtaek94 <seungtaek.kim.94@gmail.com>
2022-09-22 17:40:03 +0900

    Test

---
+++ build.bat
@@ -0,0 +1,16 @@
+
+
+mkdir onnxruntime
+cd onnxruntime
+powershell Invoke-WebRequest https://github.com/microsoft/onnxruntime/releases/download/v1.12.1/onnxruntime-win-x64-1.12.1.zip
+unzip onnxruntime-win-x64-1.12.1.zip
+dir
+cd ../
+dir
+
+git clone -b release-1.12.1 https://github.com/google/googletest.git
+
+cmake -G "Visual Studio 16 2019" -DCMAKE_BUILD_TYPE=Debug -S .\ -B .\cmake-build-debug-vs2019
+
+cmake --build .\cmake-build-debug-vs2019 --target orthandler --config Debug
+cmake --build .\cmake-build-debug-vs2019 --target test_orthandler --config Debug
