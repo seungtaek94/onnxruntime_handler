c95c7c75bdd249a98532a9943336e879d27b842e
seungtaek94 <seungtaek.kim.94@gmail.com>
2022-09-22 17:26:50 +0900

    Update linux-x64 workflow

--- build.sh
+++ build.sh
@@ -6,13 +6,9 @@ wget https://github.com/microsoft/onnxruntime/releases/download/v1.12.1/onnxrunt
 tar -zxvf onnxruntime-linux-x64-1.12.1.tgz
 cd ../
 
-echo "@@@@@@@@@@@@"
-ls -al
-
 git clone -b release-1.12.1 https://github.com/google/googletest.git
 
 cmake -DCMAKE_BUILD_TYPE=Debug -S ./ -B ./cmake-build-debug-linux
 
 cmake --build cmake-build-debug-linux/ --target orthandler -j 2
-
 cmake --build cmake-build-debug-linux/ --target test_orthandler -j 2
