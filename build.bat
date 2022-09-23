68378f465251043676649106c99bc7a906d44730
seungtaek94 <seungtaek.kim.94@gmail.com>
2022-09-23 10:13:44 +0900

    Split build action for windows and linux

--- build.bat
+++ build.bat
@@ -1,10 +1,7 @@
 mkdir onnxruntime
 cd onnxruntime
-ls
 powershell  Invoke-WebRequest https://github.com/microsoft/onnxruntime/releases/download/v1.12.1/onnxruntime-win-x64-1.12.1.zip -OutFile ".\onnxruntime-win-x64-1.12.1.zip"
-ls
 unzip .\onnxruntime-win-x64-1.12.1.zip
-ls
 cd ../
 
 git clone -b release-1.12.1 https://github.com/google/googletest.git
